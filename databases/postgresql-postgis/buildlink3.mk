7a4b3e5b16f85adf50bfbd76e2e74347f941ab90
Jonathan Perkin <jperkin@joyent.com>
2014-02-28 10:47:01 +0000

    postgis: limit dependencies to same pgsql versions.

--- databases/postgresql-postgis/buildlink3.mk
+++ databases/postgresql-postgis/buildlink3.mk
@@ -8,6 +8,8 @@ POSTGRESQL_POSTGIS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=1.4
 BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis
 
+PGSQL_VERSIONS_ACCEPTED=	91 90 84
+
 .include "../../geography/geos/buildlink3.mk"
 .include "../../geography/proj/buildlink3.mk"
 .include "../../mk/pgsql.buildlink3.mk"
